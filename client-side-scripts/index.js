8b94c066be31b91c12aa10b8195fa85b237da835
JiaqiLi000677 <59571179+JiaqiLi000677@users.noreply.github.com>
2020-04-09 15:15:19 -0300

    Add files via upload

---
+++ client-side-scripts/index.js
@@ -0,0 +1,39 @@
+// FETCHING HTML PARTIAL
+const loadBtn = document.querySelectorAll(".navigation li a");
+const loadHere = document.querySelector(".container");
+let url = "./partials/partial-1.html";
+
+
+function handleClick(e) {
+  e.preventDefault();
+  
+  let clickedLink = e.target;
+  
+  url = clickedLink.href;
+  
+  handleAjaxRequest();
+}
+
+for (let navItem of loadBtn) {
+  navItem.addEventListener("click", handleClick);
+}
+
+function handleAjaxRequest() {
+  fetch(url)
+    .then(function (response) {
+      if (response.statusText === "OK") {
+        return response.text();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(function (data) {
+      loadHere.innerHTML = data;
+    })
+    .catch(function (error) {
+      loadHere.innerHTML = `<pre>${error.name}: ${error.message}</pre>`;
+    });
+};
+
+handleAjaxRequest();
+
+
